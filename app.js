a527dc51ce8d3d2e5472fd427f7bc49f4fb9558f
Godti Vinod <venkat.godti3@gmail.com>
2021-01-07 13:19:29 +0530

    Razor pay order create implementation

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const Razorpay = require('razorpay');
+
+require('dotenv').config();
+
+const instance = new Razorpay({
+  key_id: process.env.RAZOR_PAY_KEY,
+  key_secret: process.env.RAZOR_PAY_SECRET,
+});
+
+const app = express();
+
+/* CORS setup */
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); /* *.name.domain for production  */
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  next();
+});
+app.use(cors());
+/** Body parser setup */
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+
+app.use(bodyParser.json());
+
+app.post('/create-razor-pay-order', (req, res) => {
+  const { amount, currency } = req.body;
+
+  if (!amount) {
+    res.json('Amount is required!');
+  }
+  if (!currency) {
+    res.json('Currency is required!');
+  }
+
+  const order = instance.orders.create({ amount, currency });
+  order.then((razorPayResponse) => {
+    res.json({ status: 'success', data: razorPayResponse });
+  }).catch((err) => {
+    res.json({ status: 'error', error: err });
+  });
+});
+
+app.listen(process.env.PORT, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Server running on port ${process.env.PORT}`);
+});
